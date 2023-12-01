4c6b1bccafe6f392148d5db160e8b15ddadea8d1
DongHyunTedKim <eastar29@naver.com>
2023-12-01 20:05:50 +0900

    Create a new project and explore the canvas

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ted Kim on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
