709986af9c28c039ca1d2602ca28fc8be0d4240c
TedKim <eastar29@naver.com>
2024-01-31 21:30:52 +0900

    Step1

---
+++ App/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ted Kim on 1/31/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
